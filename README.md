19146be45de290ed9b94c1988a388bffa6b64863
Berfin  Yalınız <56268192+berfin22@users.noreply.github.com>
2022-08-10 03:37:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# SpringBootGraphQL
+Java Spring Boot ile GraphQL uygulama örneği.
+
+Uygulamayı çalıştırdığımızda http://localhost:8090/graphql adresinden Graphl sorgusu atabiliriz.
+
+* Örnek sorgu: <br>
+
+- Yeni kayıt oluşturmak için,
+
+<img src="src\main\resources\1.png">
+
+- Kaydı görüntülemek için:
+
+<img src="src\main\resources\2.png">
+
---
+++ src/main/java/com/graphql/productservice/api/VehicleMutationResolver.java
@@ -0,0 +1,31 @@
+package com.graphql.productservice.api;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.graphql.productservice.dto.VehicleDto;
+import com.graphql.productservice.entity.Vehicle;
+import com.graphql.productservice.repo.VehicleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+
+@Component
+@RequiredArgsConstructor
+public class VehicleMutationResolver implements GraphQLMutationResolver {
+
+    private final VehicleRepository _vehicleRepository;
+
+    public Vehicle createVehicle(VehicleDto vehicleDto){
+        return _vehicleRepository.save(dtoToEntity(vehicleDto));
+    }
+
+    private Vehicle dtoToEntity(VehicleDto vehicleDto){
+        Vehicle vehicle=new Vehicle();
+        vehicle.setBrandName(vehicleDto.getBrandName());
+        vehicle.setLaunchDate(new Date());
+        vehicle.setModelCode(vehicleDto.getModelCode());
+        vehicle.setType(vehicleDto.getType());
+        return vehicle;
+    }
+}
